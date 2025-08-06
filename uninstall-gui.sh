950784f2b9a5d26617374e5562a0e50d55637e83
gotham-sunrise <127977785+gotham-sunrise@users.noreply.github.com>
2025-08-06 05:14:18 -0400

    Create uninstall-gui.sh

---
+++ uninstall-gui.sh
@@ -0,0 +1,8 @@
+sudo dnf remove -y \
+  gdm gnome-shell gnome-session gnome-terminal \
+  gnome-control-center gnome-classic-session \
+  gnome-initial-setup gnome-software gnome-system-monitor \
+  gnome-logs gnome-disk-utility \
+  xorg-x11* wayland gtk3 gstreamer1-plugins-good-gtk \
+  webkit2gtk3 xdg-desktop-portal \
+  chrome-gnome-shell ibus-gtk3
