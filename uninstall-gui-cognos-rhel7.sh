f56f43851a74db6b40cde8eeb3033f069c42d0c0
gotham-sunrise <127977785+gotham-sunrise@users.noreply.github.com>
2025-08-06 05:46:22 -0400

    Create uninstall-gui-cognos-rhel7.sh

---
+++ uninstall-gui-cognos-rhel7.sh
@@ -0,0 +1,10 @@
+sudo yum remove -y \
+  gnome-session gnome-shell gnome-terminal gnome-control-center \
+  gnome-initial-setup gnome-software gnome-system-monitor \
+  gnome-color-manager gnome-weather gnome-user-docs \
+  gnome-online-accounts gnome-bluetooth gnome-icon-theme \
+  gnome-packagekit-common gnome-python2-canvas gnome-python2-gconf \
+  gnome-shell-extension-* gnome-settings-daemon \
+  gtk2 gtk3 libcanberra-gtk3 libcanberra-gtk2 PackageKit-gtk3-module \
+  xorg-x11\* mutter metacity nautilus abrt-desktop \
+  at-spi2-core dbus-x11
